df41328a2f28e6d2c8e33b62ead6b93e88181c1d
bricehemery <bmbiandja@gmail.com>
2025-02-27 09:19:24 +0100

    feat: Initializing Terraform backend for Novu

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "project_id" {
+  description = "The project ID to work in"
+  type        = string
+  default     = "jack-lucius-dev"
+}
+
+variable "region" {
+  description = "The region to to deploy ressources in"
+  type        = string
+  default     = "europe-west1"
+}
