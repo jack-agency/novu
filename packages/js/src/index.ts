143662774258c1a39a3e589d0d93262f802d68f0
George Desipris <73396808+desiprisg@users.noreply.github.com>
2025-04-07 16:39:25 +0300

    fix(js,react,next): Named type exports (#8080)

--- packages/js/src/index.ts
+++ packages/js/src/index.ts
@@ -1,4 +1,18 @@
 export type { EventHandler, Events, SocketEventNames } from './event-emitter';
 export { Novu } from './novu';
-export * from './types';
+export {
+  ChannelPreference,
+  ChannelType,
+  FiltersCountResponse,
+  InboxNotification,
+  ListNotificationsResponse,
+  Notification,
+  NotificationFilter,
+  NotificationStatus,
+  NovuError,
+  NovuOptions,
+  Preference,
+  PreferenceLevel,
+  WebSocketEvent,
+} from './types';
 export { areTagsEqual, isSameFilter } from './utils/notification-utils';
